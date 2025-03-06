b9449743011587f97686f41d9943850d42bcbc77
macharia <mrmachariaorsam@gmail.com>
2025-03-06 16:25:12 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(){
+    console.log(`function exits`);
+}
+
+
+function saturdayFun(activity=`overridden`){
+    return`This Saturday, I want to bathe my dog!`
+}
+
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'special') {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
